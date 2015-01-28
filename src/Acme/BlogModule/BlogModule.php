024eb2f769c4348f3f9d3426272a48a1e01e33de
David Négrier <d.negrier@thecodingmachine.com>
2015-01-28 18:11:38 +0100

    Turning Symfony 2 module into a middleware

--- src/Acme/BlogModule/BlogModule.php
+++ src/Acme/BlogModule/BlogModule.php
@@ -6,6 +6,7 @@ use Interop\Framework\Module;
 use Interop\Framework\ModuleInterface;
 use Interop\Container\ContainerInterface;
 use Stack\UrlMap;
+use Mouf\StackPhp\SymfonyMiddleware;
 
 /**
  * The blog module is a Symfony application.
@@ -26,8 +27,9 @@ class BlogModule implements ModuleInterface
     	/* @var $stackBuilder \Stack\Builder */
     	$stackBuilder = $rootContainer->get('stackBuilder');
     	
-    	$stackBuilder->push(UrlMap::class, [
+    	/*$stackBuilder->push(UrlMap::class, [
     			'/blog' => new HttpApplication($rootContainer, 'dev', true)
-    	]);
+    	]);*/
+    	$stackBuilder->push(SymfonyMiddleware::class, new HttpApplication($rootContainer, 'dev', true));
     }
 }
